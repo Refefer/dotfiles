0171d81acc45612040f6d1ecbe98b523c72c1bea
refefer <refefer@gmail.com>
2012-12-28 10:58:44 -0500

    Adds install script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Setup .vimrc
+if [ "$0" != "./setup.sh" ]; then
+    echo "Please run from project directory"
+    exit 1
+fi
+
+# Vim
+ln -s $PWD/.vimrc ~/.vimrc
+
+# Terminator
+if [! -d ~/.config/terminator ]; then
+    mkdir -p ~/.config/terminator
+fi
+ln -s $PWD/terminator_config ~/.vimrc
+
+ln -s $PWD/.bashrc ~/.bashrc
