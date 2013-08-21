feb32584ba8014b89d46b6e66e93a819a13c3a52
Andrew Stanton <Refefer@gmail.com>
2013-08-20 23:36:12 -0400

    Adds ctags and aliases

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ fi
 
 # Vim
 ln -s $PWD/.vimrc ~/.vimrc
+ln -s $PWD/.vim ~/.vim
 
 # Terminator
 if [ ! -d ~/.config/terminator ]; then
@@ -14,5 +15,10 @@ if [ ! -d ~/.config/terminator ]; then
 fi
 ln -s $PWD/terminator_config ~/.config/terminator/config
 
+# bash
 ln -s $PWD/.bashrc ~/.bashrc
-ln -s $PWD/.vim ~/.vim
+ln -s $PWD/.bash_aliases ~/.bash_aliases
+
+
+# CTags
+ln -s $PWD/.ctags ~/.ctags
